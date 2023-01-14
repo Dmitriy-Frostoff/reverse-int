3f3c1dddb2fb63f52c626df9676d127e5a12f50f
dmitriy-frostoff <dmitriy.frostoff@yandex.ru>
2023-01-15 01:07:08 +0300

    feat: solve the task, tests pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let absNum = Math.abs(n);
+    let inputToStr = String(absNum);
+    let reverseInt = +(inputToStr.split('').reverse().join(''));
+    return reverseInt;
 }
