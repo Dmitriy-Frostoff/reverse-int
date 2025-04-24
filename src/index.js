f4fb9e5473af39fe5866f40d45cf09505de8e648
Natein <natein@gmail.com>
2025-04-24 22:08:24 +0300

    chore(task configs and data): merge all the actual task data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function reverse(n) {
-    let absNum = Math.abs(n);
-    let inputToStr = String(absNum);
-    let reverseInt = +inputToStr.split("").reverse().join("");
-    return reverseInt;
+  const absNum = Math.abs(n);
+  const inputToStr = String(absNum);
+  const reverseInt = +inputToStr.split('').reverse().join('');
+  return reverseInt;
 };
