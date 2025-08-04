5a87965bf69ac26d1de1f430b0f7d5638be588f1
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-08-04 23:28:43 +0300

    refactor(index.js): add digit's check and add JSDoc comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
+/**
+ * Reverse any finite number
+ * @note always got non - negative result
+ *
+ * @param {number} n - finite number
+ *
+ * @returns {number} - reversed number
+ *
+ *  @example
+ *    reverse(42) => 24
+ *    reverse(-42) => 24
+ *    reverse(9) => 9
+ *    reverse(0) => 0
+ *
+ */
 module.exports = function reverse(n) {
+  /** @type{number} */
   const absNum = Math.abs(n);
+
+  // digit ? => return it
+  if (absNum % 10 === absNum) {
+    return absNum;
+  }
+
+  /** @type{string} */
   const inputToStr = String(absNum);
+  /** @type{number} */
   const reverseInt = +inputToStr.split('').reverse().join('');
   return reverseInt;
 };
